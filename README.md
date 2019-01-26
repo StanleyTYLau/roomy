fff037ea97bdd714842d4ca1f92c2f225654c93b
Stanley Lau <stanley.tyl@gmail.com>
2019-01-26 19:47:50 +0000

    user route masic setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # roomy
 Web application that helps people find roommates and places to live.
+
+##Instructions
+1) Go to the 'server' folder;
+2) Run psql command in your terminal;
+3) Inside of psql run 'CREATE DATABASE roomy OWNER labber';
+4) Exit psql ('\q');
+5) Run the latest migration ('knex migrate:latest')
+6) Run seeds ('knex seed:run');
--- server/server.js
+++ server/server.js
@@ -8,28 +8,18 @@ const knex        = require("knex")(knexConfig[ENV]);
 const app = express();
 const port = process.env.PORT || 5000;
 
+//Seperate Routes for each Resource
+const usersRoutes = require("./routes/users");
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+//Mount all resource routes
+app.use("/users", usersRoutes(knex));
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
 
-  let test = "";
-  knex.select('*')
-    .from('users')
-    .then( rows => {
-      test = rows;
-      console.log(rows);
-      res.send(
-        `${rows[0].first_name} I received your POST request. This is what you sent me: ${req.body.post}`,
-      );
-    });
 
-  
-});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
