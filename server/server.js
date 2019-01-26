fda8bb9034cc8eadbd23a8fbd81260b19748b111
Stanley Lau <stanley.tyl@gmail.com>
2019-01-26 01:15:46 +0000

    knex setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const ENV         = process.env.ENV || "development";
+const knexConfig  = require("./knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,9 +17,19 @@ app.get('/api/hello', (req, res) => {
 
 app.post('/api/world', (req, res) => {
   console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+
+  let test = "";
+  knex.select('*')
+    .from('users')
+    .then( rows => {
+      test = rows;
+      console.log(rows);
+      res.send(
+        `${test[0].username}I received your POST request. This is what you sent me: ${req.body.post}`,
+      );
+    });
+
+  
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
