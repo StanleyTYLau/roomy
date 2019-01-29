70bfb83c71f0b3799473f7a456f4fd8218e8f7d0
Stanley Lau <stanley.tyl@gmail.com>
2019-01-29 21:49:21 +0000

    edit seed files to create both tables in places w/ user FK

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -1,14 +1,14 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([
-        {first_name: 'Alex', last_name: 'Peterson', email: 'alex@email.com', password: 'password1', gender: 'male', smoker: false, pets: false, cleanliness: 'High', type: 'roomy'},
-        {first_name: 'Peter', last_name: 'Parker', email: 'peter@email.com', password: 'password2', gender: 'male', smoker: true, pets: false, cleanliness: 'Moderate', type: 'roomy'},
-        {first_name: 'Math', last_name: 'Murdock', email: 'math@email.com', password: 'password3', gender: 'male', smoker: true, pets: true, cleanliness: 'Low', type: 'roomy'},
-        {first_name: 'Mary-Jane', last_name: 'Whatson', email: 'mary-jane@email.com', password: 'password4', gender: 'female', smoker: false, pets: false, cleanliness: 'Low', type: 'roomy'}
-      ]);
-    });
+  // // Deletes ALL existing entries
+  // return knex('users').del()
+  //   .then(function () {
+  //     // Inserts seed entries
+  //     return knex('users').insert([
+  //       {first_name: 'Alex', last_name: 'Peterson', email: 'alex@email.com', password: 'password1', gender: 'male', smoker: false, pets: false, cleanliness: 'High', type: 'roomy'},
+  //       {first_name: 'Peter', last_name: 'Parker', email: 'peter@email.com', password: 'password2', gender: 'male', smoker: true, pets: false, cleanliness: 'Moderate', type: 'roomy'},
+  //       {first_name: 'Math', last_name: 'Murdock', email: 'math@email.com', password: 'password3', gender: 'male', smoker: true, pets: true, cleanliness: 'Low', type: 'roomy'},
+  //       {first_name: 'Mary-Jane', last_name: 'Whatson', email: 'mary-jane@email.com', password: 'password4', gender: 'female', smoker: false, pets: false, cleanliness: 'Low', type: 'roomy'}
+  //     ]);
+  //   });
 };
