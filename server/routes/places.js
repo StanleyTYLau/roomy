6d0d4989e02347f981b9f4cf286284ffe00cb0b8
Stanley Lau <stanley.tyl@gmail.com>
2019-01-30 00:35:53 +0000

    grabed user data of place owner and dummy current user, ready for comparing

--- server/routes/places.js
+++ server/routes/places.js
@@ -12,11 +12,50 @@ module.exports = (knex) => {
   });
 
   router.get('/:id', (req, res) =>{
-    //get the place w/ params.id
-    //get the corresponding place owner profile
-    //get currebt user profile
+    dummyId = 10;//grab data from req.body current user
+    let user1 = {};
+    let user2 = {};
+    //get the place w/ params.id and get the corresponding place owner profile
+    //SELECT * FROM places JOIN users ON users.id=places.user_id;
+    knex.select('user_id')
+      .from('places')
+      .where('id', req.params.id)
+      .then( (results) => {
+        getUserInfo(results[0].user_id)
+        .then((results) => {
+          console.log(results);
+          user1 = results;
+
+          //get current user profile
+          getUserInfo(dummyId)
+          .then((results) => {
+            user2 = results
+            res.send([user1, user2]);
+
+            // [{"id":8,"first_name":"Alex","last_name":"Peterson","email":"alex@email.com","password":"password1","gender":"male","smoker":false,"pets":false,"cleanliness":"High","type":"roomy"}]
+            // [{"id":10,"first_name":"Math","last_name":"Murdock","email":"math@email.com","password":"password3","gender":"male","smoker":true,"pets":true,"cleanliness":"Low","type":"roomy"}]
+          })
+          
+        });
+
+
+        
+      })
+    
     //compare mathcing %
   });
 
+  async function getUserInfo(id) {
+    const info = await knex('users').select('*').where('id', id).returning('*');
+    return info;
+  }
+
+  function compareUsers(user1, user2) {
+    
+  }
+
   return router;
 }
+
+
+
