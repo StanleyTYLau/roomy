d782cf34c131d13c7c5b89eaf21001ce418c404e
Stanley Lau <stanley.tyl@gmail.com>
2019-01-30 20:57:12 +0000

    Matching alorithm complete w/ dummy users, appended to place info sent in response

--- server/routes/places.js
+++ server/routes/places.js
@@ -13,24 +13,54 @@ module.exports = (knex) => {
 
   router.get('/:id', (req, res) =>{
     dummyId = 10;//grab data from req.body current user
+    //DUmmy users
+    const profile1 = {
+      gender: 'male',
+      smoker: false,
+      pets: true,
+      work_sched: 'day',
+      cleanliness: 'medium',
+      go_out_freq: 'high',
+      guests_freq: 'low',
+      hobbies: ['Reading', 'Video Games'],
+      diet: 'none',
+      personality: 'introvert'
+    }
+    const profile2 = {
+      gender: 'male',
+      smoker: false,
+      pets: true,
+      work_sched: 'day',
+      cleanliness: 'medium',
+      go_out_freq: 'high',
+      guests_freq: 'low',
+      hobbies: ['Reading', 'Video Games'],
+      diet: 'none',
+      personality: 'introvert'
+    }
+
     let user1 = {};
     let user2 = {};
+    let place = {};
     //get the place w/ params.id and get the corresponding place owner profile
     //SELECT * FROM places JOIN users ON users.id=places.user_id;
-    knex.select('user_id')
+    knex.select('*')
       .from('places')
       .where('id', req.params.id)
       .then( (results) => {
+        place = results[0];
         getUserInfo(results[0].user_id)
-        .then((results) => {
-          console.log(results);
-          user1 = results;
+        .then((owner) => {
+          user1 = owner;
 
           //get current user profile
           getUserInfo(dummyId)
-          .then((results) => {
-            user2 = results
-            res.send([user1, user2]);
+          .then((currentUser) => {
+            user2 = currentUser
+
+            console.log("Matching %:", compareUsers(profile1, profile2));
+            place['matchPercent'] = compareUsers(profile1, profile2);
+            res.send(place);
 
             // [{"id":8,"first_name":"Alex","last_name":"Peterson","email":"alex@email.com","password":"password1","gender":"male","smoker":false,"pets":false,"cleanliness":"High","type":"roomy"}]
             // [{"id":10,"first_name":"Math","last_name":"Murdock","email":"math@email.com","password":"password3","gender":"male","smoker":true,"pets":true,"cleanliness":"Low","type":"roomy"}]
@@ -55,7 +85,7 @@ module.exports = (knex) => {
     let weight = {
       gender: 5,
       smoker: 5,
-      pet_owner: 5,
+      pets: 5,
       work_sched: 10,
       cleanliness: 10,
       go_out_freq: 5,
@@ -64,13 +94,17 @@ module.exports = (knex) => {
       diet: 5,
       personality: 10
     }
+    let maxScore = 0;
+    for (i in weight){
+      maxScore += weight[i];
+    }
     let scores = [];
     let finalScore = 0;
 
     //Calculate weighted score for attributes that should match 1:1
-    function _score1To1(attr1, attr2, attrWeight){
-      if(attr1 === attr2){
-        scores.push(1 * attrWeight);
+    function _score1To1(user1, user2, attrName){
+      if(user1[attrName] === user2[attrName]){
+        scores.push(1 * weight[attrName]);
       }
       else {
         scores.push(0);
@@ -78,12 +112,52 @@ module.exports = (knex) => {
     }
 
     //Calculate weighted score for attributes that have 3 options low, medium, high
-    function _score3(attr1, attr2, attrWeight){
-      if(attr1 === attr2){
-        scores.push(1 * attrWeight);
+    function _score3(user1, user2, attrName, attrWeight){
+      let user1_attr = _convertToNum(user1[attrName]);
+      let user2_attr = _convertToNum(user2[attrName]);
+      
+      switch (user1_attr - user2_attr){
+        case 0:
+          scores.push(1 * attrWeight);
+          break;
+        case 1:
+          scores.push(0.5 * attrWeight)
+          break;
+        case 2:
+          scores.push(0 * attrWeight)
+          break;
+        default:
+          return Error("Err on weighted scores calc")
       }
-      else {
-        scores.push(0);
+    }
+
+    //take array of hobbies from 2 users and calc % matching
+    function _scoreHobbies(user1, user2){
+      const total = user1.length;
+      let matches = 0;
+
+      user1.forEach( hobby1 => {
+        if (user2.indexOf(hobby1) >= 0){
+          matches++;
+        }
+      })
+
+      return (matches / total);
+    }
+
+    function _convertToNum(rank) {
+      switch (rank.toLowerCase()){
+        case "low":
+          return 0;
+          break;
+        case "medium":
+          return 1;
+          break;
+        case "high":
+          return 2;
+          break;
+        default:
+          return Error("Err: convert ranking to number"); 
       }
     }
 
@@ -91,98 +165,31 @@ module.exports = (knex) => {
       //compare each attribute
       //provide % match for the attribute
       //get weighted score for the attribute
-    _score1To1(user1.gender, user2.gender, weight.gender);
-    _score1To1(user1.smoker, user2.smoker, weight.smoker);
-    _score1To1(user1.pets, user2.pets, weight.pet_owner);
-    _score1To1(user1.work_sched, user2.work_sched, weight.work_sched);
-   
+    _score1To1(user1, user2, 'gender');
+    _score1To1(user1, user2, 'smoker');
+    _score1To1(user1, user2, 'pets');
+    _score1To1(user1, user2, 'work_sched');
+    _score1To1(user1, user2, 'diet');
+    _score1To1(user1, user2, 'personality');
+    _score3(user1, user2, 'cleanliness', weight.cleanliness);
+    _score3(user1, user2, 'go_out_freq', weight.go_out_freq);
+    _score3(user1, user2, 'guests_freq', weight.guests_freq);
   
-   
-  
-    switch (_convertToNum(user1.cleanliness) - _convertToNum(user2.cleanliness)){
-      case 0:
-        scores.push(1 * weight.cleanliness)
-        break;
-      case 1:
-        scores.push(0.5 * weight.cleanliness)
-        break;
-      case 2:
-        scores.push(0 * weight.cleanliness)
-        break;
-      default:
-        return Error("Err on weighted scores calc")
-    }
-    switch (_convertToNum(user1.go_out_freq) - _convertToNum(user2.go_out_freq)){
-      case 0:
-        scores.push(1 * weight.go_out_freq)
-        break;
-      case 1:
-        scores.push(0.5 * weight.go_out_freq)
-        break;
-      case 2:
-        scores.push(0 * weight.go_out_freq)
-        break;
-      default:
-        return Error("Err on weighted scores calc")
-    }
-    switch (_convertToNum(user1.guests_freq) - _convertToNum(user2.guests_freq)){
-      case 0:
-        scores.push(1 * weight.guests_freq)
-        break;
-      case 1:
-        scores.push(0.5 * weight.guests_freq)
-        break;
-      case 2:
-        scores.push(0 * weight.guests_freq)
-        break;
-      default:
-        return Error("Err on weighted scores calc")
-    }
-    if (user1.diet === user2.diet){
-      scores.push(1 * weight.diet)
-    }
-    if (user1.personality === user2.personality){
-      scores.push(1 * weight.personality)
-    }
     scores.push(_scoreHobbies(user1.hobbies, user2.hobbies) * weight.hobbies);
 
     //total all weighted scores for all attributes
     scores.forEach((score) => {
       finalScore += score;
     })
+    console.log("Matching scores:", scores);
+    console.log("max scores:", maxScore);
 
-    return finalScore;
+    return (finalScore / maxScore);
   }
 
-  //take array of hobbies from 2 users and calc % matching
-  function _scoreHobbies(user1, user2){
-    const total = user1.length;
-    let matches = 0;
 
-    user1.forEach( hobby1 => {
-      if (user2.indexOf(hobby1) >= 0){
-        matches++;
-      }
-    })
 
-    return (matches / total);
-  }
-
-  function _convertToNum(rank) {
-    switch (rank.toLowerCase()){
-      case "low":
-        return 0;
-        break;
-      case "medium":
-        return 1;
-        break;
-      case "high":
-        return 2;
-        break;
-      default:
-        return Error("Err: convert ranking to number"); 
-    }
-  }
+  
 
   return router;
 }
