e48570eeb78d356aa92accdeff0d247594cb62d4
Rapha321 <ralpha19@hotmail.com>
2019-01-31 17:17:04 +0000

    register

--- server/routes/places.js
+++ server/routes/places.js
@@ -12,23 +12,5 @@ module.exports = (knex) => {
   });
 
 
-router.connect((err, db, done) => {
-  if(err) {
-    return response.status(400).send(err);
-  }
-  else {
-    db.query('INSERT INTO places (postal_code, street_number, street_name, unit_number, province, city, price, type, number_of_bathrooms') VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)', [postal_code, street_number, street_name, unit_number, province, city, price, type, number_of_bathrooms], (err, table) => {
-    done();
-      if(err) {
-        return response.status(400).send(err);
-      }
-      else {
-        console.log('DATA INSERTED');
-        db.end();
-        response.status(201).send({message: "data inserted"});
-      }
-    }
-  }
-
   return router;
 }
