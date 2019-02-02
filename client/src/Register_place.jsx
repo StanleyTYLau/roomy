2ce8a1300edf60e65a423ed9ae4be3e41b1c15d3
Oleksandr09 <okatiukha@gmail.com>
2019-02-02 19:43:51 +0000

    search feature is working

--- client/src/Register_place.jsx
+++ client/src/Register_place.jsx
@@ -109,9 +109,9 @@ class Register extends React.Component {
                     <Label for="typeSelect" sm={3}>Type of Building</Label>
                     <Col sm={9}>
                       <Input type="select" name="typeSelect" id="typeSelect" onChange={this._hanldeType} >
-                        <option>Appartment Building</option>
+                        <option>Apartment</option>
                         <option>Condo</option>
-                        <option>Private House</option>
+                        <option>House</option>
                       </Input>
                     </Col>
                   </FormGroup>
@@ -298,7 +298,7 @@ class Register extends React.Component {
           streetName: this.state.streetName,
           unitNumber: this.state.unitNumber,
           neighbourhood: this.state.neighbourhood,
-          buildingType: this.state.buildingType,
+          buildingType: this.state.buildingType.toLowerCase(),
           price: this.state.price,
           description: this.state.description,
           bathrooms: this.state.bathrooms,
